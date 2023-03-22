7d56a2cc565782428e1ad57d4b19e4ca6825fec2
Almacrh <Alma_Cruz_Hernandez@outlook.com>
2023-03-22 13:59:47 +0000

    sequelize added, creating models nowand db connect done

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 require('dotenv').config()
 const morgan = require('morgan')
 const express = require('express')
-const app = express()
 
+const { Sequelize } = require('sequelize')
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.BD_HOST,
+    dialect: 'mysql',
+    port: process.env.DB_PORT,
+    logging: false
+})
+
+
+const app = express()
 const router = require('./api/routes')
 
 app.use(morgan('dev'))
 app.use(express.json())
 app.use('/api', router)
 
-app.listen(process.env.PORT, (err) => {
-        if(err) throw new Error(`ERROR: Cannot run server on port ${process.env.PORT}\n`, err)
+
+
+app.listen(process.env.PORT, async (err) => {
+    if (err) throw new Error(`ERROR: Cannot run server on port ${process.env.PORT}\n`, err)
 
     console.log(`Express listening on port ${process.env.PORT}`)
+
+    try {
+       await sequelize.authenticate()
+    } catch (error){
+        console.error('ERROR: Unable to connect to the database:', error)
+    }
+    
 })
